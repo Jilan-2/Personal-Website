47eaf3c2840c1a02ecc0c1ad6ff48ecac3c9e645
Jilan-2 <jilan9305@gmail.com>
2024-03-22 22:21:49 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+window.onload = function() {
+    var topLine = document.querySelector('.top-line');
+    var profilePicture = document.querySelector('.profile-picture');
+    var headerDescription = document.querySelector('.header-description');
+
+    topLine.style.width = '100%';
+    profilePicture.style.opacity = '1';
+    headerDescription.style.opacity = '1';
+};
+
+
+  
